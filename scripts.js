394f89843399cdd11b33ad2d796bb5d6654cc77c
MxSGameJPS <mxsgamejps@gmail.com>
2025-02-03 23:47:57 -0300

    enviando arquivo

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const uploadBtn = document.getElementById("upload-btn");
+const inputUpload = document.getElementById("image-upload");
+
+uploadBtn.addEventListener("click", () => {
+  inputUpload.click();
+});
+
+function lerConteudoDoArquivo(arquivo) {
+  return new Promise((resolve, reject) => {
+    const leitor = new FileReader();
+    leitor.onload = () => {
+      resolve({ url: leitor.result, nome: arquivo.nome });
+    };
+
+    leitor.onerror = () => {
+      reject(`Erro na leitura do arquivo ${arquivo.name}`);
+    };
+
+    leitor.readAsDataURL(arquivo);
+  });
+}
+
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+
+inputUpload.addEventListener("change", async (evento) => {
+  const arquivos = evento.target.files[0];
+
+  if (arquivos) {
+    try {
+      const conteudoDoArquivo = await lerConteudoDoArquivo(arquivos);
+      imagemPrincipal.src = conteudoDoArquivo.url;
+      nomeDaImagem.textContent = conteudoDoArquivo.nome;
+    } catch (erro) {
+      console.error(erro);
+      alert("Ocorreu um erro ao carregar a imagem.");
+    }
+  }
+});
